247ac4799782f5239ebbf46811eb20e6e2c8852e
Conrad <9323501+clarkmyfancy@users.noreply.github.com>
2022-01-22 22:06:35 -0600

    add tests and functionality to get comma count in number

--- Number Names/test_translator_helpers.py
+++ Number Names/test_translator_helpers.py
@@ -11,8 +11,15 @@ class TestTranslatorHelpers(unittest.TestCase):
         self.assertEqual(3, Helpers().get_first_digit(34509006543))
         
     def test_should_be_able_to_left_shift_number(self):
-        self.assertEqual(4509006543, Helpers().left_shift(34509006543))
+        self.assertEqual(543, Helpers().left_shift(6543))
+
+    def test_should_get_3_commas(self):
+        one_trillion = 100000000000000
+        self.assertEqual(4, Helpers().get_number_of_commas_in_number(one_trillion))
     
+    def test_should_get_0_commas(self):
+        self.assertEqual(0, Helpers().get_number_of_commas_in_number(123))
+     
 
 if __name__ == '__main__':
     unittest.main()  # pragma: no cover
--- Number Names/translator_helpers.py
+++ Number Names/translator_helpers.py
@@ -16,3 +16,16 @@ class Helpers:
     def left_shift(self, number):
         shift = math.pow(10, Helpers().number_of_digits(number) - 1)
         return number % shift
+
+    def left_shift_by_1000(self, number):
+        shift = math.pow(10, Helpers().number_of_digits(number) - 1)
+        return 
+
+    def get_number_of_commas_in_number(self, number):
+        commas = 0
+
+        number /= 1000
+        while int(number) > 0:
+            number /= 1000
+            commas += 1
+        return commas
