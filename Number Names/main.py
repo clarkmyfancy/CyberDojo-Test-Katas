9c65a0204cc36f5d83fef5bc525cc0e467701107
Conrad <9323501+clarkmyfancy@users.noreply.github.com>
2022-01-23 20:23:00 -0600

    remove report folder

--- Number Names/main.py
+++ Number Names/main.py
@@ -3,12 +3,9 @@ from translator import Translator
 def main():
 
     t = Translator()
-    # t.translate(1000000)
-    first = 0
-    second= 1
-    t.get_segment(which_segment=first, number=123456)
-    x = t.get_segment(which_segment=0, number=20000000)
-    t.translate_segment(x)
+
+    # t.translate(487230)
+    t.translate(1000000)
 
 
 if __name__ == "__main__":
